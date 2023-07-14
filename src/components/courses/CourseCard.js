64fd4708f72947746d4fa909c52a18915306912c
Krit <yothinpeace@gmail.com>
2023-07-15 01:05:09 +0700

    ENH: Add props validation

--- src/components/courses/CourseCard.js
+++ src/components/courses/CourseCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 function CourseImage({ id, title, slug }) {
   return (
@@ -20,36 +21,51 @@ function CourseImage({ id, title, slug }) {
   );
 }
 
+CourseImage.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+};
+
 function CourseDetails({ title, authorName, category, course, slug, onDeleteClick}) {
-    return (
-      <div className="speaker-info">
-        <Link to={"/collection/" + slug}>
-          <div className="d-flex justify-content-between mb-0">
-            <h3 className="text-truncate">{title}</h3>
+  return (
+    <div className="speaker-info">
+      <Link to={"/collection/" + slug}>
+        <div className="d-flex justify-content-between mb-0">
+          <h3 className="text-truncate">{title}</h3>
+        </div>
+      </Link>
+      <div>
+        <p className="card-description">{authorName}</p>
+        <div className="social d-flex flex-row mt-4">
+          <div className="company">
+            <h5>Category:</h5>
+            <h6>{category}</h6>
           </div>
-        </Link>
-        <div>
-          <p className="card-description">{authorName}</p>
-          <div className="social d-flex flex-row mt-4">
-            <div className="company">
-              <h5>Category:</h5>
-              <h6>{category}</h6>
-            </div>
-            <div>
-              <button
-                className="btn btn-outline-danger"
-                onClick={() => onDeleteClick(course)}
-              >
-                Delete
-              </button>
-            </div>
+          <div>
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => onDeleteClick(course)}
+            >
+              Delete
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  function CourseCard({ course, onDeleteClick }) {
+CourseDetails.propTypes = {
+  title: PropTypes.string.isRequired,
+  authorName: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  course: PropTypes.object.isRequired,
+  slug: PropTypes.string.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
+};
+
+function CourseCard({ course, onDeleteClick }) {
   const { id, title, authorName, category, slug } = course;
   return (
     <div className="col-xs-8 col-sm-8 col-md-6 col-lg-4 col-sm-8 col-xs-8">
@@ -67,5 +83,10 @@ function CourseDetails({ title, authorName, category, course, slug, onDeleteClic
     </div>
   );
 }
+
+CourseCard.propTypes = {
+  course: PropTypes.object.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
+};
 
 export default CourseCard;
